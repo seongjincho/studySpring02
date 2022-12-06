aedfb451d185831a439a640219471e5c8e637bb9
jason <ypc1@naver.com>
2022-12-06 17:35:26 +0900

    싱글톤 방식의 주의점2

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -1,15 +1,17 @@
 package hello.core.singleton;
 
 public class StatefulService {  //cmd + shift + t 로 테스트 만들기 가능
-    private int price; // 상태를 유지하는 필드, 10000 -> 20000원으로 바꿔짐
+    //private int price; // 상태를 유지하는 필드, 10000 -> 20000원으로 바꿔짐
 
-    public void order(String name, int price) {
+
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 여기가 문제!
-    }
-
-    public int getPrice() {
+        //this.price = price; // 여기가 문제!
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,16 +18,16 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A사용자가 10000원을 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadA: B사용자가 10000원을 주문
-        statefulService1.order("userB", 20000);
+        int userBPrice = statefulService1.order("userB", 20000);
 
         //ThreadA: 사용자A가 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+        //int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
-        // 스프링은 항상 무상태로 설계 해야한다
+        //assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        // 스프링은 항상 무상태(stateless)로 설계 해야한다
 
     }
 
