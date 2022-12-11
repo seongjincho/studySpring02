fb5764e865cde667720fd6a2fd2b5795a772d290
jason <jasonyp@daum.net>
2022-12-11 21:07:40 +0900

    수동빈 vs 자동빈

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -35,4 +38,18 @@ public class AutoAppConfig {
 //            @Controlller : 스프링 MVC 컨트롤러에서 사용
 //            @Service : 스프링 비즈니스 로직에서 사용
 //            @Repository : 스프링 데이터 접근 계층에서 사용 @Configuration : 스프링 설정 정보에서 사용
+
+    // 자동빈 vs 수동빈 충돌 테스트 ?  에러 발생하지 않는다 수동빈이 우선권을 갖고  수동빈이 자동빈을 오버라이딩한다
+    //Overriding bean definition for bean 'memoryMemberRepository' with a different definition: replacing
+//    물론 개발자가 의도적으로 이런 결과를 기대했다면, 자동 보다는 수동이 우선권을 가지는 것이 좋다. 하지만 현실은 개발자가 의도적으로 설정해서 이런 결과가 만들어지기 보다는 여러 설정들이 꼬여서 이런 결과가 만들어지는 경우가 대부분이다!
+//    그러면 정말 잡기 어려운 버그가 만들어진다. 항상 잡기 어려운 버그는 애매한 버그다.
+//    그래서 최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
+    //The bean 'memoryMemberRepository',
+    // defined in class path resource [hello/core/AutoAppConfig.class], could not be registered.
+    // A bean with that name has already been defined in file [/Users/a17647/Desktop/study/core/out/production/classes/hello/core/member/MemoryMemberRepository.class]
+    // and overriding is disabled.
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
