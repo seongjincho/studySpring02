4114345ac057270cd5947d681e0382a31e2c0533
jason <jasonyp@daum.net>
2022-12-18 23:42:26 +0900

    인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecyle/NetworkClient.java
+++ src/test/java/hello/core/lifecyle/NetworkClient.java
@@ -1,12 +1,17 @@
 package hello.core.lifecyle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    //InitializingBean 초기화 빈 , DisposableBean 종료 => 단점: 스프링에 의존적 , (초기화,소멸) 메소드명 변경 불가 , 외부 라이브러리에 적용 X
+    // 위에 방법은 초창기에 나온 방법이다.
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+        //connect();
+        //call("초기화 연결 메시작");
     }
 
     public void setUrl(String url) {
@@ -25,5 +30,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception { //초기화 후 호출
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception { // 종료 후 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
