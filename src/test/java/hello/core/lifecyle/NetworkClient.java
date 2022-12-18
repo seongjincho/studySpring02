51eef80d520850deddca13af0cd61fa8fa2d5a80
jason <jasonyp@daum.net>
2022-12-18 23:48:27 +0900

    빈 등록 초기화, 소멸 메서드 지정

--- src/test/java/hello/core/lifecyle/NetworkClient.java
+++ src/test/java/hello/core/lifecyle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecyle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient /*implements InitializingBean, DisposableBean*/ {
     //InitializingBean 초기화 빈 , DisposableBean 종료 => 단점: 스프링에 의존적 , (초기화,소멸) 메소드명 변경 불가 , 외부 라이브러리에 적용 X
     // 위에 방법은 초창기에 나온 방법이다.
     private String url;
@@ -32,16 +32,28 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { //초기화 후 호출
-        System.out.println("NetworkClient.afterPropertiesSet");
+//    @Override
+//    public void afterPropertiesSet() throws Exception { //초기화 후 호출
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//    @Override
+//    public void destroy() throws Exception { // 종료 후 호출
+//        System.out.println("NetworkClient.destroy");
+//        disconnect();
+//    }
+
+    public void init() { //초기화 후 호출
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
-
-    @Override
-    public void destroy() throws Exception { // 종료 후 호출
-        System.out.println("NetworkClient.destroy");
+    public void close() { // 종료 후 호출
+        System.out.println("NetworkClient.close");
         disconnect();
     }
+
+    //메서드 이름을 자유롭게 줄 수 있다, 스프링빈이 스프링코드에 의존하지 않는다. , 외부 라이브러리에 적용 가능
+
 }
