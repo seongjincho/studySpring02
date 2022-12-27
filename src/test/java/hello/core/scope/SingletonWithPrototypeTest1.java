f77c8af4069fc57540726b454a390cd3f06505da
jason <jasonyp@daum.net>
2022-12-28 00:06:59 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 provider로 문제해결2

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -52,11 +53,21 @@ public class SingletonWithPrototypeTest1 {
 //            this.prototypeBean = prototypeBean;
 //        }
 
+        //@Autowired
+        //private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        //ObjectProvider는 지금딱 필요한 DL정도의 기능만 제공한다 Dependency Lookup (DL) 의존관계 조회(탐색) , 스프링에 의존
+        // 그래서 나온것이 JSR-330 Provider
+        // 마지막 방법은 javax.inject.Provider 라는 JSR-330 자바 표준을 사용하는 방법이다.
+        // 이 방법을 사용하려면 javax.inject:javax.inject:1 라이브러리를 gradle에 추가해야 한다.
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
+        // 자바표준이고 기능이 단순해 단위테스트를 만들거나 mock코드를 만들기 쉽다
+
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            //PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // 항상 새로운 프로토타입 비이 생성되는 것을 확인할 수 있다.
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // 항상 새로운 프로토타입 비이 생성되는 것을 확인할 수 있다.
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
